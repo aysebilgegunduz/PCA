fd41dedcd00e864647c19b027f9ea251753f5ae5
aysebilgegunduz <aysebilgegunduz@gmail.com>
2016-11-23 16:29:12 +0300

    Mean added

--- pca_v2.py
+++ pca_v2.py
@@ -1,7 +1,19 @@
 __author__ = 'bilge'
 import numpy as np
+import pandas as pd
 from scipy import linalg as la
+# Read data files:
+train = pd.read_csv("train.csv")
+test  = pd.read_csv("test.csv")
 
-A = np.random.randint(0,10,25).reshape(5,5)
-e_vals, e_vecs = la.eig(A)
-print(e_vals,e_vecs)
+
+train_x = train.values[:,:-1]
+#A primarily label-location based indexer, with integer position fallback.
+train_y = train.ix[:,-1:]
+test_x = test.values
+mean = np.mean(train_x, axis=0)
+#x = np.vstack([a,b,c])
+#cov = np.cov(x)
+#e_vals, e_vecs = la.eig(A)
+#print(e_vals,e_vecs)
+print(mean)
