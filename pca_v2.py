fe01752c4294fd1593e19c7d25424c1e79b52c5f
aysebilgegunduz <aysebilgegunduz@gmail.com>
2016-11-23 23:19:15 +0300

    eigenvalues and eigenvectors are founded

--- pca_v2.py
+++ pca_v2.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 from scipy import linalg as la
 # Read data files:
-train = pd.read_csv("train.csv")
-test  = pd.read_csv("test.csv")
+train = pd.read_csv("train.csv", header=None)
+test  = pd.read_csv("test.csv", header=None)
 
 
 train_x = train.values[:,:-1]
@@ -12,8 +12,16 @@ train_x = train.values[:,:-1]
 train_y = train.ix[:,-1:]
 test_x = test.values
 mean = np.mean(train_x, axis=0)
-#x = np.vstack([a,b,c])
-#cov = np.cov(x)
-#e_vals, e_vecs = la.eig(A)
-#print(e_vals,e_vecs)
-print(mean)
+mean_test = np.mean(test_x, axis=0)
+meanCent = train_x - mean
+meanCentTest= test_x - mean_test
+#x = np.vstack(meanCent)
+cov = np.cov(meanCent.T)
+cov_test = np.cov(meanCentTest.T)
+e_vals, e_vecs = la.eig(cov)
+e_vals_test, e_vecs_test = la.eig(cov_test)
+#ms = sorted(enumerate(e_vals), key=lambda x: x[1], reverse=True)
+# %80 ilk 40 tanesi
+print(e_vecs.shape, e_vecs_test.shape)
+
+
